d2d56945691342786185e460dcd23f5c81be9666
japc <japc.grafico@gmail.com>
2022-05-30 18:09:18 +0200

    Queries custom fields with Tuple

--- JavaPersistence/src/com/bitcero/test/Test.java
+++ JavaPersistence/src/com/bitcero/test/Test.java
@@ -5,8 +5,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Tuple;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Root;
 
 import org.hibernate.Session;
@@ -27,23 +29,30 @@ public class Test {
 			tx = session.beginTransaction();
 
 			CriteriaBuilder builder = session.getCriteriaBuilder();
-			CriteriaQuery<Transaction> criteria = builder.createQuery(Transaction.class);
+			CriteriaQuery<Tuple> criteria = builder.createQuery(Tuple.class);
 
 			// Se definde el tipo de entidad que retorna la consulta
 			Root<Transaction> root = criteria.from(Transaction.class);
+			
+			Path<Integer> pathId = root.get(Transaction_.id);
+			Path<Timestamp> pathDate = root.get(Transaction_.date);
+			
 
 			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-			Date parseDate = dateFormat.parse("2022-05-30 17:24:11");
+			Date parseDate = dateFormat.parse("2022-05-31 00:00:00");
 
 			// Construyendo la consulta
-			criteria.select(root).where(builder.and(
+			criteria.multiselect(pathId, pathDate)
+				.where(builder.and(
 					builder.like(root.get(Transaction_.type), "%Inversion%"),
 					builder.lessThan(root.get(Transaction_.date), new Timestamp(parseDate.getTime()))
 					));
 
-			List<Transaction> transactions = session.createQuery(criteria).getResultList();
-
-			System.out.println(transactions);
+			List<Tuple> tuples = session.createQuery(criteria).getResultList();
+			
+			for (Tuple tuple : tuples) {
+				System.out.println("Tupla: " +  tuple.get(pathId) + "," + tuple.get(pathDate));
+			}
 
 			tx.commit();
 
